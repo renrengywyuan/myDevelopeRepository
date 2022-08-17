2aa4a097a8bec3d15ef319cb99ea58ecd602c466
lwy <probe_lwy@163.com>
2022-08-17 11:00:07 +0800

    测试数据

--- FisherySystem/FisherySystem/AppDelegate/AppDelegate.h
+++ FisherySystem/FisherySystem/AppDelegate/AppDelegate.h
@@ -6,7 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+// hahhahahahahha123
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic, assign) BOOL allowRotation;//这个属性标识屏幕是否允许旋转
